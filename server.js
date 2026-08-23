074500f45efca3e016aa2c8b5d576bbb70f5b46a
Bhavik Pathak <bhavikpathak08@gmail.com>
2026-08-23 23:37:52 +0530

    feat: add X-API-Version response header middleware in Express backend

--- server.js
+++ server.js
@@ -18,9 +18,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Custom Request Logger Middleware
+// Custom Request Logger & Header Middleware
 app.use((req, res, next) => {
     const timestamp = new Date().toISOString();
+    res.setHeader('X-API-Version', '1.0.0');
     console.log(`[${timestamp}] ${req.method} ${req.url}`);
     next();
 });
